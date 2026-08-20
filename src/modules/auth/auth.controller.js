6788f0794f60df92e7424a6d0b8c5501eb055f8b
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-08-21 01:05:12 +0430

    send otp fn

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,15 +1,22 @@
+const AuthMessage = require("./auth.messages");
 const authService = require("./auth.service");
+const autoBind = require('auto-bind')
 
 class AuthController {
     #service;
 
-    constructor() { 
+    constructor() {
+        autoBind(this)
         this.#service = authService
     }
 
     async sendOTP(req, res, next) {
         try {
-
+            const { mobile } = req.body;
+            await this.#service.sendOTP(mobile)
+            return {
+                message: AuthMessage.SendOtpSuccessfully
+            }
         } catch (error) {
             next(error)
         }
