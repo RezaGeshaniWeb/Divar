64ee5913ca02e06a83499f8ad0b2f57dd015e8cd
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-08-31 14:19:42 +0430

    check otp fn

--- src/modules/auth/auth.messages.js
+++ src/modules/auth/auth.messages.js
@@ -2,6 +2,9 @@ const AuthMessage = {
     SendOtpSuccessfully: 'send otp successfully',
     NotFound: 'user not found',
     OtpCodeNotExpired: 'otp code not expired, please try later',
+    OtpCodeExpired: 'otp code expired, please try to get new code',
+    OtpCodeIsIncorrect: 'otp code is incorrect',
+    LoginSuccessfully: 'your login successfully',
 }
 
 module.exports = AuthMessage;
