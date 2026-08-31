71efa0f680991cf38d3fdd0fbf418cf8a223327e
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-08-31 10:20:23 +0430

    set error handler

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require("express")
 const dotenv = require("dotenv")
 const SwaggerConfig = require("./src/config/swagger.config")
 const mainRouter = require("./src/app.routes")
+const NotFoundHandler = require("./src/common/exception/not-found.handler")
+const AllExceptionHandler = require("./src/common/exception/all-exception.handler")
 dotenv.config()
 
 async function main() {
@@ -11,6 +13,8 @@ async function main() {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     SwaggerConfig(app)
+    NotFoundHandler(app)
+    AllExceptionHandler(app)
     app.use(mainRouter)
     app.listen(port, () => console.log(`server run on port ${port}`))
 }
