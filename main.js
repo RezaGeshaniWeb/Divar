54c55929d15edb6357a06066b391037078a174e0
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-08-31 15:02:45 +0430

    save token to cookie

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const SwaggerConfig = require("./src/config/swagger.config")
 const mainRouter = require("./src/app.routes")
 const NotFoundHandler = require("./src/common/exception/not-found.handler")
 const AllExceptionHandler = require("./src/common/exception/all-exception.handler")
+const cookieParser = require("cookie-parser")
 dotenv.config()
 
 async function main() {
@@ -12,6 +13,7 @@ async function main() {
     require("./src/config/mongoose.config")
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
+    app.use(cookieParser(process.env.COOKIE_SECRET_KEY))
     SwaggerConfig(app)
     app.use(mainRouter)
     NotFoundHandler(app)
--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,3 +1,4 @@
+const NodeEnv = require("../../common/constant/env.enum");
 const AuthMessage = require("./auth.messages");
 const authService = require("./auth.service");
 const autoBind = require('auto-bind')
@@ -26,9 +27,11 @@ class AuthController {
         try {
             const { mobile, code } = req.body;
             const token = await this.#service.checkOTP(mobile, code)
-            return res.json({
-                message: AuthMessage.LoginSuccessfully,
-                token
+            return res.cookie("access_token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === NodeEnv.Production
+            }).status(200).json({
+                message: AuthMessage.LoginSuccessfully
             })
         } catch (error) {
             next(error)
