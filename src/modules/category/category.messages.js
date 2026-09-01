e7fbf6f7fb1db3c6e5f7446c80156c1b1ed32558
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-01 12:19:20 +0430

    create category controller

---
+++ src/modules/category/category.messages.js
@@ -0,0 +1,5 @@
+const CategoryMessage = Object.freeze({
+    Created: "category created successfully"
+})
+
+module.exports = CategoryMessage;
---
+++ src/modules/category/category.service.js
@@ -0,0 +1,21 @@
+const autoBind = require("auto-bind");
+const CategoryModel = require("./category.model");
+
+class CategoryService {
+    #model
+
+    constructor() {
+        autoBind(this)
+        this.#model = CategoryModel;
+    }
+
+    async create(categoryDto) {
+
+    }
+    
+    async find() {
+
+    }
+}
+
+module.exports = new CategoryService()
