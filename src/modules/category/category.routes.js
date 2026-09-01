b75cad1150185685db295abfc7aa82b07d90235e
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-01 11:52:37 +0430

    create category swagger

---
+++ src/modules/category/category.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express")
+
+const router = Router()
+
+router.post('/create', () => {})
+router.get('/all', () => {})
+
+module.exports = {
+    CategoryRouter: router
+}
