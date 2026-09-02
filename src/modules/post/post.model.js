f6edc099ccccbd07f7be9e5881ba40ff0f685171
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-02 10:10:13 +0430

    create post model

---
+++ src/modules/post/post.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const PostSchema = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    category: { type: Types.ObjectId, ref: "Category", required: true },
+    province: { type: String, required: true },
+    city: { type: String, required: true },
+    district: { type: String, required: true },
+    cordinate: { type: [Number], required: true },
+    images: { type: [String], required: false, default: [] },
+})
+
+const PostModel = model("Post", PostSchema)
+
+module.exports = PostModel;
