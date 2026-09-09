7cf48ce5753a3003d7ff17bce1ed19f73bf31671
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-10 03:24:33 +0430

    show dashboard content

--- src/app.routes.js
+++ src/app.routes.js
@@ -11,7 +11,7 @@ mainRouter.use('/user', UserRouter)
 mainRouter.use('/category', CategoryRouter)
 mainRouter.use('/option', OptionRoutes)
 mainRouter.get("/", (req, res) => {
-    res.render("./pages/index.ejs")
+    res.render("./pages/panel/dashboard.ejs")
 })
 
 module.exports = mainRouter;
