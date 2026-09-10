fe436ae7c6d1520745f93f16a287c895c2ce1ab4
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-10 22:56:44 +0430

    create post form

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -1,6 +1,5 @@
 const autoBind = require("auto-bind");
 const PostMessage = require("./post.messages");
-const httpCodes = require("http-codes");
 const postService = require("./post.service");
 const CategoryModel = require("../category/category.model");
 const createHttpError = require("http-errors");
@@ -18,22 +17,22 @@ class PostController {
             let { slug } = req.query
             let showBack = false
             let match = { parent: null }
-            let categories = []
+            let options;
             if (slug) {
                 slug = slug.trim()
                 const category = await CategoryModel.findOne({ slug })
                 if (!category) throw new createHttpError.NotFound(PostMessage.NotFound)
+                options = await this.#service.getCategoryOptions(category._id)
+                if (options.length === 0) options = null;
                 showBack = true
                 match = {
                     parent: category._id
                 }
-            } else {
-
             }
-            categories = await CategoryModel.aggregate([{
+            const categories = await CategoryModel.aggregate([{
                 $match: match
             }])
-            res.render("./pages/panel/create-post.ejs", { categories, showBack })
+            res.render("./pages/panel/create-post.ejs", { categories, showBack, options })
         } catch (error) {
             next(error)
         }
